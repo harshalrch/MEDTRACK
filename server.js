e78a45cbba3ebdf352c7fef64f826b26a6f4bc83
harshalrch <harshalchinchole10@gmail.com>
2026-04-21 19:21:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors     = require('cors');
 const path     = require('path');
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
